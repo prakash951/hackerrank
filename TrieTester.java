e847d764b2820608a946ada3b7449f0af6538804
Prakash <prakash.barri@gmail.com>
2015-11-19 23:21:23 -0500

    Update TrieTester.java

--- TrieTester.java
+++ TrieTester.java
@@ -1,11 +1,7 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
-
-class Ideone {
+public class Solution {
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -13,28 +9,20 @@ class Ideone {
         String result = "";
         String resultstr = "";
         while(n-- > 0)
-            {
-            String str = br.readLine();
-            trie.insert(str);
-            if(trie.flag)
-            {
-                if(result=="")
-                result = "BAD SET";
-                if(resultstr=="")
-                resultstr = str;
-                break;
-            }
+        {
+                String str = br.readLine();
+                if(!trie.find(str,trie.root))
+                {
+                     trie.insert(str);
+                }
+                else
+                {
+                    System.out.println("BAD SET");
+                    System.out.println(str);
+                    return;
+                }
         }
-        if(result!="")
-            {
-            System.out.println(result);
-            System.out.println(resultstr);
-        }
-        else
-            {
-            System.out.println("GOOD SET");
-        }
-        trie.traverse(trie.root);
+        System.out.println("GOOD SET");
     }
 }
 
@@ -50,58 +38,37 @@ class Trie
         insertUtil(str,root);
     }
     public boolean flag = false;
+    
+    public boolean find(String str,TrieNode root)
+    {
+        if(root==null) return false;
+        for(int i=0;i<str.length();i++)
+        {
+            int z = atoi(str.charAt(i));
+            if(root.nodes[z]==null)
+            {
+                return false;
+            }
+            if(root.nodes[z].word==true)
+                {
+                return true;
+            }
+            root = root.nodes[z];
+        }
+        return true;
+    }
     public void insertUtil(String str,TrieNode root)
         {
         for(int i=0;i<str.length();i++)
             {
-        		int z = atoi(str.charAt(i));
-                if(z!=-1)
-                {
+                    int z = atoi(str.charAt(i));
                     if(root.nodes[z]==null)
                     {
                         root.nodes[z] = new TrieNode(str.charAt(i));
                     }
                     root = root.nodes[z];
-                    if(root.word==true)
-                    {
-                        flag = true;
-                    }
-                }
-                /**else
-                    {
-                    flag = true;
-                }*/
         }
         root.word = true;
-    }
-    
-    public void traverse(TrieNode root)
-    {
-    	if(root==null)
-    		return;
-    	char[] ch = new char[1024];
-    	traverseUtil(root,ch,0);
-    	
-    }
-    
-    
-    public void traverseUtil(TrieNode root,char[] ch,int index)
-    {
-    	if(root==null) return;
-    	if(root.word)
-    	{
-    		System.out.println(new String(ch,0,index));
-    		return;
-    	}
-    	for(int i=0;i<32;i++)
-    	{
-    		if(root.nodes[i]!=null)
-    		{
-    			ch[index] = root.nodes[i].data;
-    			traverseUtil(root.nodes[i],ch,index+1);
-    		}
-    	}
-    	
     }
     
     public int atoi(char ch)
